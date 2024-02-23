1ca9ee9a60a6883835d6e8e41e6569c6fcbca247
DEVnitishOfficial <nitish.naroun123@gmail.com>
2024-02-23 22:57:46 +0530

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-    <h1 className="text-5xl text-center">
+   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+     <h1 className="mt-3 text-2xl text-center">
      Hello Mr. DEVnitishOfficial 
     </h1>
+   </main>
   );
 }
