53ecf8b5b9bad160538d7120ef7bbcd4dee48d0d
DEVnitishOfficial <nitish.naroun123@gmail.com>
2024-02-24 23:08:36 +0530

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-     <h1 className="mt-3 text-2xl text-center">
-     Hello Mr. DEVnitishOfficial 
-    </h1>
+    
+    <HeroSection />
    </main>
   );
 }
