0ba6dbeef364d8abc9fbb9e20a6876e02d26d3f5
DEVnitishOfficial <nitish.naroun123@gmail.com>
2024-03-01 21:33:02 +0530

    added featured card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import StickyScroll from "@/components/ImplementStickyScroll";
 import TestimonialCard from "@/components/TestimonialCard";
+import UpcomingWebminar from "@/components/UpcomingWebminar";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
     <FeaturedCourses/>
     <StickyScroll />
     <TestimonialCard />
+    <UpcomingWebminar />
    </main>
   );
 }
