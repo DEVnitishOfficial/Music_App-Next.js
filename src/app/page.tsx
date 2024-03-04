7e4937b4790ad4853e79f236c20efd7ba23f8238
DEVnitishOfficial <nitish.naroun123@gmail.com>
2024-03-04 12:10:22 +0530

    added contact corse and footer page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import StickyScroll from "@/components/ImplementStickyScroll";
 import Instructor from "@/components/Instructor";
 import TestimonialCard from "@/components/TestimonialCard";
 import UpcomingWebminar from "@/components/UpcomingWebminar";
+import MusicSchoolContactUs from "./contact/page";
 
 export default function Home() {
   return (
@@ -15,6 +17,7 @@ export default function Home() {
     <TestimonialCard />
     <UpcomingWebminar />
     <Instructor />
+    <Footer />
    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,6 +60,14 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       keyframes:{
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
         spotlight: {
           '0%': { opacity: '0', transform: 'translate(-72%, -62%) scale(0.5)' },
           '100%': { opacity: '1', transform: 'translate(-50%,-40%) scale(1)' },
